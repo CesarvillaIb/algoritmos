244b5a02e82c7b22a3d446f4e4dd585954cda1fa
paulo villa Ibarra <za19011260@zapopan.tecmm.edu.mx>
2024-01-29 23:39:39 -0600

    primer compromiso

---
+++ tarea1.py
@@ -0,0 +1,9 @@
+def main():
+
+    nombre = input("Ingrese su nombre: ")
+    numero_control = input("Ingrese su número de control: ")
+    print("\nNombre:   ", nombre)
+    print("Número de control:  ", numero_control)
+
+if __name__ == "__main__":
+    main()
